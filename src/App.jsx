ddb9b9b1d51131609c35bf70af489a492b269f9b
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 19:19:11 +0200

    get exchange rates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 function App() {
-  let rates = {
-    EUR: 1,
-    AED: '3.973561',
-  }
+  let [currency, setCurrency] = useState('')
+  let [rates, setRates] = useState({})
 
   return (
     <>
-      <form>
-        currency: <input />
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          axios
+            .get(`https://open.er-api.com/v6/latest/${currency}`)
+            .then((response) => setRates(response.data.rates))
+        }}
+      >
+        <label>
+          currency:{' '}
+          <input
+            type="text"
+            value={currency}
+            onChange={(event) => setCurrency(event.target.value)}
+          />
+        </label>
         <button type="submit">exchange rate</button>
       </form>
+      <div>
+        <a href="https://www.exchangerate-api.com/docs/supported-currencies">
+          Supported currencies
+        </a>
+      </div>
       <pre>{JSON.stringify(rates, null, 2)}</pre>
     </>
   )
